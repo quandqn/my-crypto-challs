217ee09904f497cddc2bb2b5eee77ec2d2f0b719
Quan Doan <jos.quandqn@gmail.com>
2018-11-19 15:30:54 +0700

    Add files via upload

---
+++ svattt-2018/final/jwt/__init__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# flake8: noqa
+
+"""
+JSON Web Token implementation
+
+Minimum implementation based on this spec:
+http://self-issued.info/docs/draft-jones-json-web-token-01.html
+"""
+
+
+__title__ = 'pyjwt'
+__version__ = '0.4.3'
+__author__ = 'José Padilla'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2015 José Padilla'
+
+
+from .api import encode, decode, register_algorithm
+from .exceptions import (
+    InvalidTokenError, DecodeError, ExpiredSignatureError,
+    InvalidAudienceError, InvalidIssuerError,
+    ExpiredSignature, InvalidAudience, InvalidIssuer
+)
